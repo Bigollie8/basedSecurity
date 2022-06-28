e7bab8444340c9e9fd18c7053664574149d33977
mellyl0l <83857421+mellyl0l@users.noreply.github.com>
2022-06-28 11:35:52 -0400

    fix dis shit

--- PHP_Databases/basedSecurity-update.php
+++ PHP_Databases/basedSecurity-update.php
@@ -33,7 +33,7 @@ if (strlen($_POST['vendorID']) > 5) {
 if (strlen($_POST['deviceID']) > 5) {
     die(json_encode($response));
 }
-
+//make this better kinda sucks and can be exploited
 $clientEncrypt = $_POST['encryption'];
 $username = $_POST['username'];
 $uid = $_POST['uid'];
