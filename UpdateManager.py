267fb00739b5b22eaa8ac6871d92b3ba9db2e401
Bigollie8 <86810528+Bigollie8@users.noreply.github.com>
2022-07-15 16:21:59 -0500

    test

--- UpdateManager.py
+++ UpdateManager.py
@@ -3,10 +3,10 @@ import sys
 import os
 from turtle import update
 from git import Repo
+import git
 import shutil
 import datetime 
 from ftplib import FTP
-from io import StringIO
 
 def constructPath():
     # This function is used to construct a folder path used to store
@@ -21,7 +21,7 @@ def constructPath():
         suffix = "am"
 
     timestamp = str(rawDate.year) + "-" + str(rawDate.month) + "-" + str(rawDate.day)  + " " + hour  + "-" + str(rawDate.minute)  + "-" + str(rawDate.second) + suffix
-    parent = r'C:/Users/bigol/Desktop/Update Manager/root'
+    parent = r'C:\Users\bigol\Desktop\Update Manager\root'
     return os.path.join(parent, timestamp)
     
 path = constructPath()
@@ -51,17 +51,7 @@ def pause():
 
 list_of_files = []
 
-def listFiles():
-    # This function will take the local save of the security and 
-    # push the update to the cpanel. This will keep us accountable
-    # to our github commit and gives a more orginized feeling to 
-    # pushing updates 
 
-    for root, dirs, files in os.walk(path):
-        for file in files:
-            if "\.git" not in file:
-                print("Appending " + str(file) + " to payload")
-                list_of_files.append(os.path.join(root,file))
 
 def githubRepo():
     # This function pulls the files from our github when you intiate the 
@@ -101,16 +91,35 @@ def verifyConnection():
         ftp.quit()
     return True
 
+def checkFile(file):
+    if "backend" not in file: return False
+    if "php" not in file: return False
+    if "lua" in file: return False
+
+    return True
+
+def listFiles():
+    # This function will take the local save of the security and 
+    # push the update to the cpanel. This will keep us accountable
+    # to our github commit and gives a more orginized feeling to 
+    # pushing updates 
+
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            if "php" in file:
+                print("Appending " + str(file) + " to payload")
+                list_of_files.append(os.path.join(root,file))
+
 def updateFiles(files):
-    print("Updating files")
     ftp = FTP('162.0.228.203')
     ftp.login("baseddepartment", "+cvssATXEc@7")
     ftp.cwd('public_html')
     for x in files:
-        if ".git" not in x:
-            print("We would be deleting " + x.replace(path,""))
+        if checkFile(x):
+            serverPath = os.path.join("public_html",x.replace(path + r"\backend",""))
+            print("We would be deleting " + serverPath)
+            ftp.storlines('STOR ' + serverPath.replace("\\",""), open(x, 'rb'))
     ftp.quit()
-
 
 def driver():
     for x in range(5): startupText()
