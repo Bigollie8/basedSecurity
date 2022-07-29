39efa5e22e3c6d66946f0a1c7c5b68cddd900f60
Bigollie8 <86810528+Bigollie8@users.noreply.github.com>
2022-07-29 11:05:49 -0500

    Start of pull request

--- [Source] Loader.lua
+++ [Source] Loader.lua
@@ -1,6 +1,6 @@
 -- Security version 3.1.4
 -- Developed by Ollie#0069 & .audit#1111 (aka Melly)
-
+-- Start of pull request
 --#region Important vars
 local http                      = require("gamesense/http") or error("Sub to https://gamesense.pub/forums/viewtopic.php?id=19253 on the lua workshop.")
 local ffi                       = require("ffi") or error("0x20 Contact admin")
